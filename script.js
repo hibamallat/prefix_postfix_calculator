2683f961ddf2544d1e1ee0e27aa8e6dcebc2765f
hibamallat <hibamallat040@gmail.com>
2024-12-20 16:19:11 +0200

    implement some functions in JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+const display = document.getElementById("result");
+const equals = document.getElementById("equals");
+const space = document.getElementById("space");
+const backspace = document.getElementById("backspace");
+const clear = document.getElementById("clear");
 
+function appendToDisplay(input) {
+  display.value += input;
+}
 
+function clearExpression() {
+  display.value = "";
+}
 
+function addSpace() {
+  display.value += " ";
+}
 
+function backSpace() {
+  display.value = display.substr(0, display.length - 1);
+}
+
+clear.addEventListener("click", clearExpression);
+space.addEventListener("click", addSpace);
+backspace.addEventListener("click", backSpace);
